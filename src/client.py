a36f0a0abb3ae59dc273652d390b5bca2a0f11e3
Joao Gebara (Ubuntu) <joaopedrogebara@gmail.com>
2025-04-18 21:49:19 -0300

    feat: Inicia Chat Cliente/Servidor com socket e multithreading

--- src/client.py
+++ src/client.py
@@ -1,21 +1,79 @@
 import socket
 import threading
 
+SERVER_PORT = 12000
+SERVER_ADDRESS = '127.0.0.1'
 
-def encrypt(msg):
+def encrypt(msg: str) -> str:
     '''
         Criptografa a mensagem.
-    '''
-    pass
 
-def decrypt(msg):
+        param msg: Mensagem a ser criptografada
+        type msg: str
+        return: Mensagem criptografada
+        rtype: str
+    '''
+    return msg
+
+def decrypt(msg: str) -> str:
     '''
         Decriptografa a mensagem.
+
+        param msg: Mensagem a ser decriptografada
+        type msg: str
+        return: Mensagem decriptografada
+        rtype: str
     '''
-    pass
+    return msg
 
 def run_client():
-    pass
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client.connect((SERVER_ADDRESS, SERVER_PORT))
+    except Exception as e:
+        return print(f"Erro ao conectar ao servidor: {e}")
+
+    print("Conectado ao servidor.")
+
+
+def send_messages(client) -> None:
+    '''
+        Envia mensagens para o servidor.
+
+        param client: socket do cliente
+        type client: socket.socket
+        return: None
+    '''
+    while True:
+        msg = input("Digite sua mensagem: ")
+        if msg == 'quit':
+            break
+
+        encrypted_msg = encrypt(msg)
+
+        try:
+            client.send(encrypted_msg.encode('utf-8'))
+        except Exception as e:
+            return print(f"Erro ao enviar mensagem: {e}")
+
+def receive_messages(client) -> None:
+    '''
+        Recebe mensagens do servidor.
+
+        param client: socket do cliente
+        type client: socket.socket
+        return: None
+    '''
+    while True:
+        try:
+            msg = client.recv(2048).decode('utf-8')
+
+            decrypted_msg = decrypt(msg)
+            print(decrypted_msg + '\n')
+
+        except Exception as e:
+            return print(f"Erro ao receber mensagem: {e}")
+
 
 if __name__ == "__main__":
     run_client()
